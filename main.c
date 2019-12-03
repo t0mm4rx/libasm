e438d4d92a6a4125189167b5990dbf22c03bb2ca
t0mm4rx <tom.marx@hotmail.fr>
2019-12-04 00:12:54 +0100

    makefile, linux/macos comp

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int	ft_strlen(char *);
+int	ft_strlen(char *str);
 char	*ft_strcpy(char *dst, const char *src);
 int	ft_strcmp(const char *s1, const char *s2);
-int	ft_write(int, const void*, size_t);
-int	ft_read(int, void*, size_t);
-char	*ft_strdup(const char*);
-int	ft_atoi_base(const char*, const char*);
+int	ft_write(int fd, const void *content, size_t length);
+int	ft_read(int fd, void *buffer, size_t length);
+char	*ft_strdup(const char *src);
+int	ft_atoi_base(const char *src, const char *base);
 
 int main()
 {
