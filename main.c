af6d9e8be36c1f1ebc6eac90b6083e5153d04652
Tom <tom.marx@hotmail.fr>
2020-01-17 10:18:40 +0100

    better main

--- main.c
+++ main.c
@@ -3,53 +3,64 @@
 #include <libc.h>
 #include <unistd.h>
 
-int	ft_strlen(char *str); //done
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+int		ft_strlen(char *str);
 char	*ft_strcpy(char *dst, const char *src);
-int	ft_strcmp(const char *s1, const char *s2); //done
-int	ft_write(int fd, const void *content, size_t length); //done
-int	ft_read(int fd, void *buffer, size_t length); //done
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_write(int fd, const void *content, size_t length);
+int		ft_read(int fd, void *buffer, size_t length);
 char	*ft_strdup(const char *src);
+
+void print_header(char *str)
+{
+	printf("\n%s==> %s%s\n", GREEN, str, RESET);
+}
 
 int main()
 {
-	printf("==> Tests libasm <==\n");
+	print_header("libasm <==\n");
 
-	printf("\n** Testing ft_write:\n");
-	ft_write(1, "Hello, world\n", 13);
+	print_header("ft_write");
+	ft_write(1, "Hello, world!\n", 14);
 	ft_write(1, "", 1);
 	ft_write(1, NULL, 0);
 	ft_write(1, "I love Assembly\n", 3);
 	ft_write(1, "\n", 1);
 
-	printf("\n** Testing ft_strlen:\n");
+	print_header("ft_strlen");
 	printf("'hello': (%d, %lu)\n", ft_strlen("hello"), strlen("hello"));
 	printf("'': (%d, %lu)\n", ft_strlen(""), strlen(""));
 	printf("'Ok !\\0Hey !': (%d, %lu)\n", ft_strlen("Ok !\0Hey !"), strlen("Ok !\0Hey !"));
 
-	printf("\n** Testing ft_strcmp:\n");
+	print_header("ft_strcmp");
 	printf("'', '': (%d, %d)\n", ft_strcmp("", ""), strcmp("", ""));
 	printf("'Ok', 'Okok': (%d, %d)\n", ft_strcmp("Ok", "Okok"), strcmp("Ok", "Okok"));
 	printf("'abc', 'abd': (%d, %d)\n", ft_strcmp("abc", "abd"), strcmp("abc", "abd"));
+	printf("'abd', 'abc': (%d, %d)\n", ft_strcmp("abd", "abc"), strcmp("abd", "abc"));
 
-	printf("\n** Testing ft_strcpy:\n");
+	print_header("ft_strcpy");
 	const char *src = "Hello world!";
+	printf("Source = %p %s\n", src, src);
 	char *dest = calloc(20, sizeof(char));
 	ft_strcpy(dest, src);
-	printf("%s\n", dest);
+	printf("%p = %s\n", dest, dest);
 	strcpy(dest, src);
-	printf("%s\n", dest);
+	printf("%p = %s\n", dest, dest);
 	free(dest);
 
-	printf("\n** Testing ft_strdup:\n");
+	print_header("ft_strdup");
 	char *src2 = "Hello world!";
 	char *res = ft_strdup(src2);
-	printf("|%p %p %s %lu|\n", src2, res, res, strlen(res));
+	printf("Source = %p %s\n", src2, src2);
+	printf("|%p %s %lu|\n", res, res, strlen(res));
 	free(res);
 	res = strdup(src2);
-	printf("|%p %p %s %lu|\n", src2, res, res, strlen(res));
+	printf("|%p %s %lu|\n", res, res, strlen(res));
 	free(res);
 
-	printf("\n** Testing ft_read:\n");
+	print_header("ft_read");
 	char buffer[256] = {0};
 	ft_read(0, buffer, 256);
 	ft_write(1, buffer, 256);
--- main_bonus.c
+++ main_bonus.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <libc.h>
 
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 typedef struct		s_list
 {
 	void		*data;
@@ -19,12 +22,13 @@ t_list	*new_list(void *data)
 
 void	print_list(t_list *node)
 {
+	printf("LIST : ");
 	while (node)
 	{
 		printf("%s --> ", node->data);
 		node = node->next;
 	}
-	printf("NULL");
+	printf("NULL\n");
 }
 
 int	cmp(void *a, void *b)
@@ -46,19 +50,24 @@ void	free_el(t_list *el)
 
 size_t	ft_list_size(t_list*);
 void	ft_list_push_front(t_list**, void*);
-int	ft_atoi_base(char*, const char*);
+int		ft_atoi_base(char*, const char*);
 void	ft_swap(t_list*, t_list*);
 void	ft_list_sort(t_list**, int (*cmp)(void*, void*));
 void	ft_list_remove_if(t_list**, void*, int (*cmp)(void*, void*), void (*free)(t_list*));
 
 void	free_elem(t_list*, void (*free_node)(t_list*));
-int	cmp_elem(void*, void*, int(*cmp)(void*, void*));
+int		cmp_elem(void*, void*, int(*cmp)(void*, void*));
+
+void print_header(char *str)
+{
+	printf("\n%s==> %s%s\n", GREEN, str, RESET);
+}
 
 int main()
 {
-	printf("Tests bonus\n");
+	print_header("Tests bonus <==");
 
-	printf("\n** Testing ft_atoi_base:\n");
+	print_header("ft_atoi_base");
 	printf("|%d|\n", ft_atoi_base("17", "01234567989ABCDEF"));
 	printf("|%d|\n", ft_atoi_base("CF", "01234567989ABCDEF"));
 	printf("|%d|\n", ft_atoi_base("1000110", "01"));
@@ -66,24 +75,26 @@ int main()
 	printf("|%d|\n", ft_atoi_base("0", "01 "));
 	printf("|%d|\n", ft_atoi_base("  --++- 45102", "012345"));
 
-	printf("\n** Testing ft_list_size:\n");
+	print_header("ft_list_size");
 	t_list *node = new_list("First !");
 	node->next = new_list("Second");
 	node->next->next = new_list("Last");
 	print_list(node);
 	printf("\nLength: %zu\n", ft_list_size(node));
-	printf("Length of empty list: %zu\n", ft_list_size(NULL));
+	printf("Length of NULL: %zu\n", ft_list_size(NULL));
 
-	printf("\n** Testing ft_list_push_front:\n");
+	print_header("ft_list_push_front");
+	printf("List before push_front :\n");
+	print_list(node);
 	ft_list_push_front(&node, "Added");
+	printf("List after push_front of 'Added' :\n");
 	print_list(node);
 	t_list *empty = NULL;
-	ft_list_push_front(&empty, "Added to empty list");
-	printf("\n");
+	ft_list_push_front(&empty, "Element");
+	printf("'Element' added to an NULL list :\n");
 	print_list(empty);
-	printf("\n");
 
-	printf("\n** Testing ft_list_sort:\n");
+	print_header("ft_list_sort");
 	t_list *list;
 	ft_list_push_front(&list, "5");
 	ft_list_push_front(&list, "3");
@@ -94,28 +105,28 @@ int main()
 	ft_list_push_front(&list, "7");
 	ft_list_push_front(&list, "9");
 	ft_list_push_front(&list, "8");
+	printf("Source list :\n");
 	print_list(list);
-	printf("\n");
 	ft_list_sort(&list, &cmp);
-	//ft_swap(list, list->next);
+	printf("List sorted :\n");
 	print_list(list);
-	printf("\n");
+	// Segfault test
 	ft_list_sort(&empty, NULL);
 
-	printf("\n** Testing remove if:\n");
+	print_header("ft_remove_if");
+	printf("Source list :\n");
 	print_list(list);
-	printf("\n\nRemove '1':\n");
-	ft_list_remove_if(NULL, NULL, NULL, NULL);
+	printf("Remove '1':\n");
 	ft_list_remove_if(&list, "1", &cmp2, &free_el);
 	print_list(list);
-	printf("\n\nRemove '5':\n");
+	printf("Remove '5':\n");
 	ft_list_remove_if(&list, "5", &cmp2, &free_el);
 	print_list(list);
-	printf("\n\nRemove '9':\n");
+	printf("Remove '9':\n");
 	ft_list_remove_if(&list, "9", &cmp2, &free_el);
 	print_list(list);
-	//t_list *el = new_list("ok");
-	//free_elem(el, &free_el);
+	// Segfault test
+	ft_list_remove_if(NULL, NULL, NULL, NULL);
 
 	return (0);
 }
